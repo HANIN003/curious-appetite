b71741c3f4bc6facd52e519973eef8dce4e3d490
Ann <ann.hixson@uconn.edu>
2023-01-05 19:23:50 -0500

    added comment functionality and consolidated files

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -2,31 +2,33 @@ const router = require('express').Router();
 const { User, Recipe, Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
 
-router.get('/', async (req, res) => {
-    try {
-      const commentData = await Comment.findAll({
-        include:  [
-          {
-            model: User,
-            attributes: ['name'],
-          },
-          {
-            model: Recipe,
-            attributes: ['title']
-          }
-        ]
-      });
-      res.status(200).json(commentData);
-    } catch (err) {
-      res.status(500).json(err);
-    }
-});
+// USEFUL TO CHECK HOW THINGS ARE WORKING
+// router.get('/', async (req, res) => {
+//     try {
+//       const commentData = await Comment.findAll({
+//         include:  [
+//           {
+//             model: User,
+//             attributes: ['name'],
+//           },
+//           {
+//             model: Recipe,
+//             attributes: ['title']
+//           }
+//         ]
+//       });
+//       res.status(200).json(commentData);
+//     } catch (err) {
+//       res.status(500).json(err);
+//     }
+// });
 
 router.post('/', withAuth, async (req, res) => {
   try {
     const newComment = await Comment.create({
       ...req.body,
       user_id: req.session.user_id,
+      recipe_id: req.body.recipeId
     });
 
     res.status(200).json(newComment);
