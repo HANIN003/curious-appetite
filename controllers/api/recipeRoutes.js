1dd977ac61d4ac9b52fbf74c09bef70df3aaefeb
David Proto <davidproto@sbcglobal.net>
2023-01-08 13:57:13 -0500

    styled navbar, recipe text, and added footer

--- controllers/api/recipeRoutes.js
+++ controllers/api/recipeRoutes.js
@@ -1,51 +1,37 @@
-const router = require('express').Router();
-const { User, Recipe, Tag, RecipeTag, Comment } = require('../../models');
-const withAuth = require('../../utils/auth');
+const router = require("express").Router();
+const { User, Recipe, Tag, RecipeTag, Comment } = require("../../models");
+const withAuth = require("../../utils/auth");
 
-router.get('/', async (req, res) => {
-    try {
-      const recipeData = await Recipe.findAll({
-        include: [
-          {
-            model: User,
-            attributes: ['name'],
-          },
-          {
-            model: Tag,
-            attributes: ['tag_name'],
-            through: {
-              attributes: []
-            }
-          },
-          {
-            model: Comment,
-            attributes: ['content'],
-            include: [
-              {
-                model: User,
-                attributes: ['name']
-              }
-            ]
-          }
-        ]
-      });
-      res.status(200).json(recipeData);
-    } catch (err) {
-      res.status(500).json(err);
-    }
-});
-
-router.post('/', withAuth, async (req, res) => {
+router.get("/", async (req, res) => {
   try {
-    const newRecipe = await Recipe.create({
-      ...req.body,
-      user_id: req.session.user_id,
-      tag_id: [req.body.tag_ids]
+    const recipeData = await Recipe.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ["name"],
+        },
+        {
+          model: Tag,
+          attributes: ["tag_name"],
+          through: {
+            attributes: [],
+          },
+        },
+        {
+          model: Comment,
+          attributes: ["content"],
+          include: [
+            {
+              model: User,
+              attributes: ["name"],
+            },
+          ],
+        },
+      ],
     });
-
-    res.status(200).json(newRecipe);
+    res.status(200).json(recipeData);
   } catch (err) {
-    res.status(400).json(err);
+    res.status(500).json(err);
   }
 });
 
